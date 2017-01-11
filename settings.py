3dd7ac743f70f2d09f08729b4b3bde70d3b2dc85
codemonkey143 <codemonkey143@users.noreply.github.com>
2017-01-11 12:50:42 -0800

    settings.py

---
+++ settings.py
@@ -0,0 +1,31 @@
+global stock
+
+stock = {
+            "racing":1100,
+            "folding":700,
+            "BMX":900,
+            "human-powered":150,
+            "motor-assisted":900,
+            "mountain-biking":1000}
+        
+global weight
+weight = {
+            "racing":20,
+            "folding":30,
+            "BMX":25,
+            "human-powered":10,
+            "motor-assisted":45,
+            "mountain-biking":15
+            
+        }
+        
+global updatedStock
+updatedStock = {}
+
+global customer_sheet
+customer_sheet = {}
+
+global totalProfit
+totalProfit = []
+global selled_bicycles
+selled_bicycles = []
