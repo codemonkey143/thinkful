cfcb9755eedc5e65f02109bb7db4ef0f85cbe7e4
uday <uurouthu@gmail.com>
2017-01-15 04:10:22 +0000

    adding two more modules

--- tbay.py
+++ tbay.py
@@ -8,17 +8,26 @@ session = Session()
 Base = declarative_base()
 
 from datetime import datetime
-
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime,Float
 
 class Item(Base):
+    
     __tablename__ = "items"
-
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     
+class User(Base):
+    __tablename__ = "usermodel"
+    id = Column(Integer,primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     
-Base.metadata.create_all(engine)
+class Bid(Base):
+    __tablename__ = "bidmodel"
+    id = Column(Integer,primary_key=True)
+    price = Column(Float,nullable=False)
+    
 
+Base.metadata.create_all(engine)
